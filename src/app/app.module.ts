af449ff2d3e2dfd23bf183fd2a79bfae17076d9f
Darlan Damasio <damasio34@gmail.com>
2020-08-12 00:36:56 -0300

    Criação do componente todo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule
+    BrowserAnimationsModule,
+    FormsModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './models/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todo: Todo = new Todo();
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(todo: Todo) {
+    this.todos.push(todo);
+    this.todo = new Todo();
+    this.todos = Object.assign([], this.todos);
+  }
+
+  delete(todo: Todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
+  }
+
+}
